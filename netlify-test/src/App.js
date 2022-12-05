dfca49bcd5d2caaae58b7a308aa63ae5d11e0a9f
Robin <lawn.tee.robin@gmail.com>
2022-12-05 16:15:09 +0000

    made small changes to site

--- netlify-test/src/App.js
+++ netlify-test/src/App.js
@@ -3,7 +3,13 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Awesome job Robin!</h1>
+      <h1>Cat in Pan!</h1>
+      <img
+        src="https://live.staticflickr.com/65535/51498235126_9ff83106ed_b.jpg"
+        alt="Cat in a pan"
+        width="600"
+        height="400"
+      ></img>
     </div>
   );
 }
